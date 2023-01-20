e502e1737c169c7a1a4e4c9b53a9169790280f95
Eetu Pienimaa <eetu.pienimaa@gmail.com>
2023-01-20 15:41:54 +0200

    Further improvement

---
+++ build/precache-manifest.713b1d5ce6dc17042a804d6a99c50bcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "de7301e5fca55bdf96b8",
+    "url": "/static/css/main.7efb466f.chunk.css"
+  },
+  {
+    "revision": "de7301e5fca55bdf96b8",
+    "url": "/static/js/main.de7301e5.chunk.js"
+  },
+  {
+    "revision": "78297386d19b9b60a559",
+    "url": "/static/css/1.549df39c.chunk.css"
+  },
+  {
+    "revision": "78297386d19b9b60a559",
+    "url": "/static/js/1.78297386.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c1543ede5477c45ef58e8d1d8aee2d72",
+    "url": "/static/media/young-man-sportswear-exercise-class-gym.c1543ede.jpg"
+  },
+  {
+    "revision": "e5fa3d5e8eedd80acd5f690cd12d5af5",
+    "url": "/index.html"
+  }
+];
